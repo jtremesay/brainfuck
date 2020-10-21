02ac07c149b9f63b8d38d309f26225eb63efeaf9
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2020-10-21 04:56:17 +0200

    add some backend

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::env;
 use std::fs;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
 use std::str;
 
 /// A brainfuck token
@@ -132,6 +135,13 @@ fn optimize_ast(ast: &Node) -> Node {
     }
 }
 
+pub fn compile_source(source: &str) -> Node {
+    let ast = build_ast(parse_source(source));
+    let ast = optimize_ast(&ast);
+
+    ast
+}
+
 /// State of the brainfuck VM
 pub struct State {
     pub memory: [u8; 30000],
@@ -163,36 +173,169 @@ pub fn run_ast(node: &Node, state: &mut State) {
     }
 }
 
-/// Run a brainfuck source
-pub fn run_source(source: &str) {
-    let ast = build_ast(parse_source(source));
-    let ast = optimize_ast(&ast);
-    run_ast(
-        &ast,
-        &mut State {
-            memory: [0; 30000],
-            index: 0,
-        },
-    );
+fn write_bf(ast: &Node, write: &mut dyn Write) {
+    match ast {
+        Node::Incr(val) => {
+            for _ in 0..val.abs() {
+                if *val < 0 {
+                    write.write(b"-").unwrap();
+                } else {
+                    write.write(b"+").unwrap();
+                }
+            }
+        }
+        Node::Move(val) => {
+            for _ in 0..val.abs() {
+                if *val < 0 {
+                    write.write(b"<").unwrap();
+                } else {
+                    write.write(b">").unwrap();
+                }
+            }
+        }
+        Node::Write => {
+            write.write(b".").unwrap();
+        }
+        Node::Loop(node) => {
+            write.write(b"[").unwrap();
+            write_bf(&node, write);
+
+            write.write(b"]").unwrap();
+        }
+        Node::Block(nodes) => {
+            for node in nodes.iter() {
+                write_bf(&node, write);
+            }
+        }
+    }
+}
+
+fn write_c_ast(ast: &Node, write: &mut dyn Write) {
+    match ast {
+        Node::Incr(val) => {
+            write
+                .write(format!("    memory[index] += {};\n", val).as_bytes())
+                .unwrap();
+        }
+
+        Node::Move(val) => {
+            write
+                .write(format!("    index += {};\n", val).as_bytes())
+                .unwrap();
+        }
+        Node::Write => {
+            write
+                .write(b"    printf(\"%c\", memory[index]);\n")
+                .unwrap();
+        }
+        Node::Loop(node) => {
+            write.write(b"    while (memory[index] != 0) {\n").unwrap();
+            write_c_ast(node, write);
+            write.write(b"    }").unwrap();
+        }
+        Node::Block(nodes) => {
+            for node in nodes.into_iter() {
+                write_c_ast(node, write);
+            }
+        }
+    }
+}
+
+fn write_c(ast: &Node, write: &mut dyn Write) {
+    write.write(b"#include <stdint.h>\n").unwrap();
+    write.write(b"#include <stdio.h>\n").unwrap();
+    write.write(b"#include <stdlib.h>\n").unwrap();
+    write.write(b"\n").unwrap();
+    write
+        .write(b"int main(int argc, char ** argv) {\n")
+        .unwrap();
+    write.write(b"    uint8_t memory[30000] = {0};\n").unwrap();
+    write.write(b"    size_t index = 0;\n").unwrap();
+    write.write(b"\n").unwrap();
+    write.write(b"    // bf source code\n").unwrap();
+    write_c_ast(ast, write);
+    write.write(b"\n").unwrap();
+    write.write(b"\n").unwrap();
+    write.write(b"    return EXIT_SUCCESS;\n").unwrap();
+    write.write(b"}\n").unwrap();
+}
+
+fn write_rust_ast(ast: &Node, write: &mut dyn Write) {
+    match ast {
+        Node::Incr(val) => {
+            write
+                .write(
+                    format!(
+                        "    memory[index] = (memory[index] as isize + {}) as u8;\n",
+                        val
+                    )
+                    .as_bytes(),
+                )
+                .unwrap();
+        }
+
+        Node::Move(val) => {
+            write
+                .write(format!("    index = (index as isize + {}) as usize;\n", val).as_bytes())
+                .unwrap();
+        }
+        Node::Write => {
+            write
+                .write(b"    print!(\"{}\", memory[index] as char);\n")
+                .unwrap();
+        }
+        Node::Loop(node) => {
+            write.write(b"    while memory[index] != 0 {\n").unwrap();
+            write_rust_ast(node, write);
+            write.write(b"    }").unwrap();
+        }
+        Node::Block(nodes) => {
+            for node in nodes.into_iter() {
+                write_rust_ast(node, write);
+            }
+        }
+    }
+}
+
+fn write_rust(ast: &Node, write: &mut dyn Write) {
+    write.write(b"fn main() {\n").unwrap();
+    write
+        .write(b"    let mut memory: [u8; 30000] = [0; 30000];\n")
+        .unwrap();
+    write.write(b"    let mut index: usize = 0;\n").unwrap();
+    write.write(b"\n").unwrap();
+    write.write(b"    // bf source code\n").unwrap();
+    write_rust_ast(ast, write);
+    write.write(b"}\n").unwrap();
 }
 
 fn usage() {
     println!("brainfuck - A brainfuck compiler");
     println!("");
-    println!("usage: brainfuck input_source");
+    println!("usage: brainfuck options... input_source [output_file]");
     println!("");
+    println!("    -e, --eval      evaluate the source code");
     println!("    input_source    path to the input source");
+    println!("    output_file     path to the output file, if needed");
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut i = 1;
     let mut source_path = None;
+    let mut output_path = None;
+    let mut evaluate = false;
     while i < args.len() {
         if args[i] == "-h" || args[i] == "--help" {
             usage();
 
             return;
+        }
+
+        if args[i] == "-e" || args[i] == "--eval" {
+            evaluate = true;
+            i += 1;
+            continue;
         }
 
         if source_path.is_none() {
@@ -201,16 +344,51 @@ fn main() {
             continue;
         }
 
+        if output_path.is_none() {
+            output_path = Some(&args[i]);
+            i += 1;
+            continue;
+        }
+
         i += 1;
     }
 
-    if source_path.is_none() {
-        println!("Error: missing input source file");
-        usage();
-        return;
-    }
-
+    // Read the input source
     let source_data = fs::read(source_path.unwrap()).unwrap();
     let source = str::from_utf8(&source_data).unwrap();
-    run_source(source);
+
+    // Compile the source
+    let ast = compile_source(source);
+
+    // Run the program, if needed
+    if evaluate {
+        run_ast(
+            &ast,
+            &mut State {
+                index: 0,
+                memory: [0; 30000],
+            },
+        );
+    }
+
+    // Output the program
+    if let Some(path) = output_path {
+        let path = PathBuf::from(path);
+        let extension = path.extension().unwrap();
+        match extension.to_str().unwrap() {
+            "bf" => {
+                let mut file = File::create(path).unwrap();
+                write_bf(&ast, &mut file);
+            }
+            "c" => {
+                let mut file = File::create(path).unwrap();
+                write_c(&ast, &mut file);
+            }
+            "rs" => {
+                let mut file = File::create(path).unwrap();
+                write_rust(&ast, &mut file);
+            }
+            _ => panic!("unsupported extension {:?}", extension),
+        };
+    }
 }
