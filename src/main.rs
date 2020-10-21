6bb939981cd643446060f9e279d94e1583c045a2
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2020-10-21 02:38:10 +0200

    read source from command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+use std::fs;
+use std::str;
+
 /// A brainfuck token
 #[derive(PartialEq)]
 pub enum Token {
@@ -172,42 +176,41 @@ pub fn run_source(source: &str) {
     );
 }
 
+fn usage() {
+    println!("brainfuck - A brainfuck compiler");
+    println!("");
+    println!("usage: brainfuck input_source");
+    println!("");
+    println!("    input_source    path to the input source");
+}
+
 fn main() {
-    let source = "
-++++++++               Set Cell #0 to 8
-[
-    >++++               Add 4 to Cell #1; this will always set Cell #1 to 4
-    [                   as the cell will be cleared by the loop
-        >++             Add 2 to Cell #2
-        >+++            Add 3 to Cell #3
-        >+++            Add 3 to Cell #4
-        >+              Add 1 to Cell #5
-        <<<<-           Decrement the loop counter in Cell #1
-    ]                   Loop till Cell #1 is zero; number of iterations is 4
-    >+                  Add 1 to Cell #2
-    >+                  Add 1 to Cell #3
-    >-                  Subtract 1 from Cell #4
-    >>+                 Add 1 to Cell #6
-    [<]                 Move back to the first zero cell you find; this will
-                        be Cell #1 which was cleared by the previous loop
-    <-                  Decrement the loop Counter in Cell #0
-]                       Loop till Cell #0 is zero; number of iterations is 8
+    let args: Vec<String> = env::args().collect();
+    let mut i = 1;
+    let mut source_path = None;
+    while i < args.len() {
+        if args[i] == "-h" || args[i] == "--help" {
+            usage();
 
-The result of this is:
-Cell No :   0   1   2   3   4   5   6
-Contents:   0   0  72 104  88  32   8
-Pointer :   ^
+            return;
+        }
 
->>.                     Cell #2 has value 72 which is 'H'
->---.                   Subtract 3 from Cell #3 to get 101 which is 'e'
-+++++++..+++.           Likewise for 'llo' from Cell #3
->>.                     Cell #5 is 32 for the space
-<-.                     Subtract 1 from Cell #4 for 87 to give a 'W'
-<.                      Cell #3 was set to 'o' from the end of 'Hello'
-+++.------.--------.    Cell #3 for 'rl' and 'd'
->>+.                    Add 1 to Cell #5 gives us an exclamation point
->++.                    And finally a newline from Cell #6
-";
-    //println!("// {}", source);
+        if source_path.is_none() {
+            source_path = Some(&args[i]);
+            i += 1;
+            continue;
+        }
+
+        i += 1;
+    }
+
+    if source_path.is_none() {
+        println!("Error: missing input source file");
+        usage();
+        return;
+    }
+
+    let source_data = fs::read(source_path.unwrap()).unwrap();
+    let source = str::from_utf8(&source_data).unwrap();
     run_source(source);
 }
